bd5fa5239369acb00900e5b9519efaf0428f188b
CoachNooB <CoachNooB@users.noreply.github.com>
2020-04-05 21:33:27 +0700

    Updates

---
+++ precache-manifest.9ec28088710b8d9b099b426eba883fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe61f14b04e3a5246ff854663b005d5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2bc51d9913547106d6bd",
+    "url": "/robofriends/static/css/main.c93ba506.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc51d9913547106d6bd",
+    "url": "/robofriends/static/js/main.6a2706b5.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
